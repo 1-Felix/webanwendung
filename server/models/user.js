a1d9e04181c355b75fc63223e85e498cae77b225
Felix <felixkeller98@gmail.com>
2019-10-20 17:12:37 +0200

    Sichergestellt, dass das Passwort nicht in Plain-Text abgespeichert wird (mit bcrypt)

--- server/models/user.js
+++ server/models/user.js
@@ -24,6 +24,40 @@ const userShema = new mongoose.Schema({
     }
 });
 
+// "Pre save hook" Before ein Document (= Instanz von einem Model => User) in mongoose abgespeichert wird, 
+// wird das Passwort von User noch gehasht (mit bcrypt), damit das Passwort nicht im Plain-Text auf der Datenbank liegt.
+// Ich verwende eine Async-Function weil bcrypt.hash asynchron ist.
+// Quelle: https://www.thepolyglotdeveloper.com/2019/02/hash-password-data-mongodb-mongoose-bcrypt/
+userSchema.pre("save", async function(next){
+    try {
+        if(!this.isModified("password")){
+            return next();
+        }
+        // bcrypt arbeitet auch mit Salt -> "10" ist der "Salt Work Factor"
+        // https://stackoverflow.com/questions/4443476/optimal-bcrypt-work-factor
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch(err){
+        // falls was schiefgeht, geht's an ErrorHandler
+        return next(err);
+    }
+})
+
+// Diese Methode ist nachher für den Login nützlich damit man 
+// schauen kann ob das eingegebene Passwort das richtige ist.
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+    try {
+        // Vergleich von Übvergebenen Passwort und Passwort des Users in der Datenbank
+        // bcrypt.compare gibt True/False zurück
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch(err) {
+        return next(err);
+    }
+}
+
+
 // Jedes erstellte Objekt von diesem Schema wird durch ein Model erstellt
 // https://mongoosejs.com/docs/models.html
 const User = mongoose.model("User", userSchema);
